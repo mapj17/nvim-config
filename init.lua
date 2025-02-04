c22b66526c79256ff780a2bc43a298d8666e1033
Markus Petersson <marpet0909@gmail.com>
2025-02-04 16:54:32 +0100

    Add treesitter

--- init.lua
+++ init.lua
@@ -25,7 +25,19 @@ local plugins = {
     {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
       dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+    },
+    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "python"},
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+},
 }
 local opts = {}
 require("lazy").setup(plugins, opts)
