b9c005184ed256ce58d1033e4da55c2bff49b5df
Markus Petersson <marpet0909@gmail.com>
2025-02-05 17:20:04 +0100

    Truncate long paths in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
+    require("telescope").setup({defaults = { path_display = { "truncate" }}})
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<C-p>", builtin.find_files, {})
     --requires ripgrep
