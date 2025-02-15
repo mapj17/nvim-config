1bef8fab4ceb9b290fa50ee8591f5175d9bfbaa5
Markus Petersson <marpet0909@gmail.com>
2025-02-15 09:03:19 +0100

    Disable some lsp keybindings that clash with snacks

--- lua/lsp-buf-keybindings.lua
+++ lua/lsp-buf-keybindings.lua
@@ -1,7 +1,7 @@
 --Put in a reparate file to be loaded last. Hover did not seem to work otherwise.
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, {})
-vim.keymap.set("n", "<leader>fd", vim.lsp.buf.definition, {})
-vim.keymap.set("n", "<leader>fi", vim.lsp.buf.implementation, {})
-vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+--vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+--vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+--vim.keymap.set("n", "<leader>h", vim.lsp.buf.hover, {})
+--vim.keymap.set("n", "<leader>fd", vim.lsp.buf.definition, {})
+--vim.keymap.set("n", "<leader>fi", vim.lsp.buf.implementation, {})
+--vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
