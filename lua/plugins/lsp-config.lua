fa4634895aeda2cac1e3e38219f7be81bb9a24f2
Markus Petersson <marpet0909@gmail.com>
2025-02-23 22:19:52 +0100

    set maxlinelength to 120 in python

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,27 +1,35 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function ()
-        require("mason").setup({})
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function ()
-        --pylsp required python3-venv to be installed
-        --stylua had to be installed manually
-        require("mason-lspconfig").setup({ensure_installed = {"lua_ls", "pylsp"}})
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-        local lspconfig = require("lspconfig")
-        lspconfig.lua_ls.setup({})
-        lspconfig.pylsp.setup({})
-        lspconfig.clangd.setup({})
-        end
-    }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup({})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			--pylsp required python3-venv to be installed
+			--stylua had to be installed manually
+			require("mason-lspconfig").setup({ ensure_installed = { "lua_ls", "pylsp" } })
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.pylsp.setup({
+				settings = {
+					pylsp = {
+						plugins = {
+							pycodestyle = {
+								maxLineLength = 120,
+							},
+						},
+					},
+				},
+			})
+			lspconfig.clangd.setup({})
+		end,
+	},
 }
-
-
