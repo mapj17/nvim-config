c817e730638bf4945b437661e338be509568d3d3
Markus Petersson <marpet0909@gmail.com>
2025-02-25 22:18:28 +0100

    Add some keybindings for neorg

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -20,7 +20,26 @@ return {{
 
         vim.wo.foldlevel = 99
         vim.wo.conceallevel = 2
+
+        -- Easy enter, easy return
+        vim.keymap.set("n", "<Localleader><Localleader>n", ":Neorg index<CR>")
+        vim.keymap.set("n", "<Localleader><Localleader>r", ":Neorg return<CR>")
+
+        -- Tasks
+        vim.keymap.set("n", "<Localleader><Localleader>t", "i* ( ) <ESC>")
+        vim.keymap.set("n", "<Localleader><Localleader>ct", "yypV:s/<Bslash>(.*(.)<Bslash>).*/<Bslash>1 /g<CR>:nohl<CR>$")
+        vim.keymap.set("n", "<Localleader><Localleader>st", "yypV:s/<Bslash>(.*(.)<Bslash>).*/<Bslash>1 /g<CR>:nohl<CR>I*<ESC>$")
+
+        -- Lists
+        vim.keymap.set("n", "<Localleader><Localleader>l", "i* <ESC>")
+        vim.keymap.set("n", "<Localleader><Localleader>cl", "yypV:s/<Bslash>(.*<Bslash>*<Bslash>).*/<Bslash>1 /g<CR>:nohl<CR>$")
+        vim.keymap.set("n", "<Localleader><Localleader>sl", "yypV:s/<Bslash>(.*<Bslash>*<Bslash>).*/<Bslash>1 /g<CR>:nohl<CR>I*<ESC>$")
+
+        -- Enumerations
+        vim.keymap.set("n", "<Localleader><Localleader>e", "i~ <ESC>")
+        vim.keymap.set("n", "<Localleader><Localleader>ce", "yypV:s/<Bslash>(.*<Bslash>~<Bslash>).*/<Bslash>1 /g<CR>:nohl<CR>$")
+        vim.keymap.set("n", "<Localleader><Localleader>se", "yypV:s/<Bslash>(.*<Bslash>~<Bslash>).*/<Bslash>1 /g<CR>:nohl<CR>I~<ESC>$")
+
       end,
     }
   }
-
