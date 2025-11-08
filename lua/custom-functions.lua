31eac9b2646b5c9bbe7f79d10902750398e7a723
Markus Petersson <marpet0909@gmail.com>
2025-11-08 12:14:21 +0100

    Fix error handling for expand_op

--- lua/custom-functions.lua
+++ lua/custom-functions.lua
@@ -4,8 +4,8 @@ end
 
 function expand_op()
 	local current_line = vim.api.nvim_get_current_line()
-	if current_line:match("[-+*]=") then
-		_, _, spaces, lhs, operator, rest = current_line:find("(%s*)(%w+)%s*([-+*])=(.*)")
+	local spaces, lhs, operator, rest = current_line:match("^(%s*)([%w_]+)%s*([%-%+%*])=(.*)")
+    if spaces then
 		vim.api.nvim_set_current_line(spaces .. lhs .. " = " .. lhs .. " " .. operator .. rest)
 	else
 		vim.notify("Line does not match pattern")
