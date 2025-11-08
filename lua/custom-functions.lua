75fa05c5de027f03a4a02608a497f299372c7a52
Markus Petersson <marpet0909@gmail.com>
2025-11-08 11:43:59 +0100

    Add fcn for expanding +=, -= and *=

--- lua/custom-functions.lua
+++ lua/custom-functions.lua
@@ -1,3 +1,15 @@
 function P(arg)
-    print(vim.inspect(arg))
+	print(vim.inspect(arg))
 end
+
+function expand_op()
+	local current_line = vim.api.nvim_get_current_line()
+	if current_line:match("-=") or current_line:match("+=") or current_line:match("*=") then
+		_, _, spaces, lhs, operator, rest = current_line:find("(%s*)(%w+)%s*([-+*])=(.*)")
+		vim.api.nvim_set_current_line(spaces .. lhs .. " = " .. lhs .. " " .. operator .. rest)
+	else
+		vim.notify("Line does not match pattern")
+	end
+end
+
+vim.keymap.set("n", "<leader>+", ":lua expand_op()<CR>")
