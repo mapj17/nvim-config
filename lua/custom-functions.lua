c63b1f630c84de6c1f03ea2abc04fdded3de703e
Markus Petersson <marpet0909@gmail.com>
2025-11-08 12:02:41 +0100

    Refactor expand_op

--- lua/custom-functions.lua
+++ lua/custom-functions.lua
@@ -4,7 +4,7 @@ end
 
 function expand_op()
 	local current_line = vim.api.nvim_get_current_line()
-	if current_line:match("-=") or current_line:match("+=") or current_line:match("*=") then
+	if current_line:match("[-+*]=") then
 		_, _, spaces, lhs, operator, rest = current_line:find("(%s*)(%w+)%s*([-+*])=(.*)")
 		vim.api.nvim_set_current_line(spaces .. lhs .. " = " .. lhs .. " " .. operator .. rest)
 	else
